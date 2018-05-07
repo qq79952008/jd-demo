ba611432e468f06ded62e4b3ad908104bb95cdb3
Yangf <79952008@qq.com>
2018-05-07 22:23:28 +0800

    nav-0.2

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 $(function(){
     $(".navl").bind("mouseover",function(){
         $(".navl-dropdown").css("display","block");
-    })
+    });
     $(".navl").bind("mouseout",function(){
         $(".navl-dropdown").css("display","none");
-    })
+    });
+    $(".item").bind("click", function (e) {
+        $(".item").removeClass("item-active");
+        $(".add-name").html($(this).text());
+        $(this).addClass("item-active");
+        $(".navl-dropdown").css("display","none");
+
+    });
 })
